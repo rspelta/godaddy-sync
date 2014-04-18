cdcbd332e55dd013ec0c6529e2f0eb26065722a5
rspelta <roberto@roberto-HP.(none)>
2014-04-18 15:48:24 +0200

    godaddy-sync.sh v0.1

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 godaddy-sync
 ============
+
+Salva il repository di godaddy in locale.
+Lanciare il comando con la seguente sintassi:
+
+    ./godaddy-sync password_godaddy /path/to/save
+
+Oppure lanciare direttamente:
+
+    ./godaddy-sync
+
+e inserire i dati richiesti
+
+Per essere eseguito richiede i pacchetti "zenity" e "sshpass"
+
---
+++ godaddy-sync.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# daddu-sync.sh [password] [destination folder]
+
+if [ $# -eq 2 ]
+then
+    PASSWORD=${1}
+    LOCAL_ROOT=${2}
+else
+    PASSWORD=`zenity --password`
+    LOCAL_ROOT=`zenity --entry --text "Path of destination"`
+fi
+
+function exit_error()
+{
+    echo "ERROR"
+    exit 1
+}
+
+
+function rsync_commonboard_godaddy()
+{
+    REMOTE_DIR=${1}
+    LOCAL_DIR=${1}
+    echo -n "${LOCAL_DIR}..."
+    mkdir -p ${LOCAL_DIR}
+    sshpass -p ${PASSWORD} rsync -e ssh -az --delete-after b8800814@www.rsr-solutions.net:html/architech/${REMOTE_DIR}/ ${LOCAL_ROOT}/${LOCAL_DIR}
+    [ $? -eq 0 ] || { exit_error; }
+    echo "OK"
+}
+
+function rsync_pengwy_godaddy()
+{
+    echo -n "pengwyn..."
+    REMOTE_DIR="pengwyn"
+    LOCAL_DIR="pengwyn"
+    mkdir -p ${LOCAL_DIR}/script
+    sshpass -p ${PASSWORD} rsync -e ssh -az --delete-after b8800814@www.rsr-solutions.net:html/architech/${REMOTE_DIR}/script/ ${LOCAL_ROOT}/${LOCAL_DIR}/script
+    [ $? -eq 0 ] || { exit_error; }
+    mkdir -p ${LOCAL_DIR}/sdk
+    sshpass -p ${PASSWORD} rsync -e ssh -az --delete-after b8800814@www.rsr-solutions.net:html/architech/${REMOTE_DIR}/script/ ${LOCAL_ROOT}/${LOCAL_DIR}/sdk
+    [ $? -eq 0 ] || { exit_error; }
+    mkdir -p ${LOCAL_DIR}/toolchain
+    sshpass -p ${PASSWORD} rsync -e ssh -az --delete-after b8800814@www.rsr-solutions.net:html/architech/${REMOTE_DIR}/script/ ${LOCAL_ROOT}/${LOCAL_DIR}/toolchain
+    [ $? -eq 0 ] || { exit_error; }
+}
+
+echo "start, save in ${LOCAL_ROOT}"
+
+
+rsync_commonboard_godaddy "hachiko"
+rsync_commonboard_godaddy "hachiko-tiny"
+rsync_commonboard_godaddy "tibidabo"
+rsync_commonboard_godaddy "zedboard"
+rsync_pengwy_godaddy
+
+echo "end success"
+
+exit 0
+
